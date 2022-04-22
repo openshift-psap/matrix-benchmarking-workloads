416f20e8e9966e5dc739843a4f11e3afc2926240
Kevin Pouget <kpouget@redhat.com>
2022-04-22 10:38:01 +0200

    plot/__init__.py: improve the plotting

--- runai/plot/__init__.py
+++ runai/plot/__init__.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import statistics as stats
 import datetime
+import logging
 
 import plotly.graph_objs as go
 import plotly.subplots
@@ -47,13 +48,14 @@ class ComparisonPlot():
         slices = [1]
         ref_groups = set()
         ref_values = dict(training=None, inference=None)
+        max_values = dict(training=0, inference=0)
         ref_keys = {}
 
         for entry in common.Matrix.all_records(params, param_lists):
             inference_part = f"{entry.params.inference_count} x inference at {int(float(entry.params.inference_fraction)*100)}%"
             training_part = f"{entry.params.training_count} x training at {int(float(entry.params.training_fraction)*100)}%"
 
-            if params['partionner'] in ("sequential", "native"):
+            if params['partionner'] == "sequential":
                 group_slice = 1
             else:
                 group_slice = (int(entry.params.training_count) + int(entry.params.inference_count))
@@ -66,18 +68,27 @@ class ComparisonPlot():
             else:
                 group_name = f"{inference_part}<br>{training_part}"
 
-            group_name += f"<br>{1/group_slice*100:.0f}% of the GPU per Pod"
+            group_name += f"<br>{1/group_slice*100:.0f}% of the GPU/Pod"
+
+            if params['partionner'] == "sequential":
+                if int(entry.params.inference_count) > 1 or  int(entry.params.training_count) > 1:
+                    group_name += "<br>(sequential)"
 
             if variables:
                 remaining_args = "<br>".join([f"{key}={entry.params.__dict__[key]}" for key in variables])
                 group_name += "<br>" + remaining_args
 
+            full_gpu = False
             group_name = f"{group_name}"
-            if params['partionner'] == "native":
+            if params['partionner'] == "sequential":
+                full_gpu = True
+
                 if entry.results.training_speed:
-                    group_name += "<br>(training reference)"
+                    if entry.params.training_count == "1":
+                        group_name += "<br>(<b>training reference</b>)"
                 else:
-                    group_name += "<br>(inference reference)"
+                    if entry.params.inference_count == "1":
+                        group_name += "<br>(<b>inference reference</b>)"
 
             group_names.append(group_name)
 
@@ -89,24 +100,30 @@ class ComparisonPlot():
                     if mode_name == "inference":
                         legend_name = f"Inference #{idx}"
                         value = sum(values)/len(values)
+                        max_values["inference"] = max([max_values["inference"], value])
                     else:
                         legend_name = f"Training #{idx}"
                         value = values
-
+                        max_values["training"] = max([max_values["training"], value])
+                    if full_gpu:
+                        legend_name += " (full)"
                     ref_keys[legend_name] = mode_name
                     legend_names.add(legend_name)
                     results[legend_name][group_name] = value
                     group_legends[group_name].append(legend_name)
 
-            if params["partionner"] == "native":
+            if params["partionner"] == "sequential":
                 for key in ref_values:
                     if entry.params.__dict__[f"{key}_count"] == "1":
-                        if ref_values[key] is not None: print(f"WARNING: found multiple {key} reference values (prev: {ref_values[key]}), new: {value} ...")
+                        if ref_values[key] is not None:
+                            logging.warning(f"Found multiple {key} reference values (prev: {ref_values[key]}), new: {value} ...")
+
                         ref_values[key] = value
                         ref_groups.add(group_name)
 
         fig = plotly.subplots.make_subplots(specs=[[{"secondary_y": True}]])
         x_labels = []
+        max_extra_ref_pct = 0
         for legend_name in sorted(legend_names):
             x_labels = []
             x_idx = []
@@ -120,11 +137,10 @@ class ComparisonPlot():
 
                 if "x inference" not in group_name:
                     sort_index += 100
-                if "reference" not in group_name:
-                    sort_index += 200
                     #if "1 x training" in group_name: sort_index += 50
+                if "training at 100%" in group_name:
+                    sort_index += 50
 
-                print(f"{sort_index:04d} {group_name}")
                 return f"{sort_index:04d} {group_name}"
 
             group_names.sort(key=sort_key)
@@ -164,7 +180,7 @@ class ComparisonPlot():
                     slices = group_slices[group_name]
                     ref = ref_values[ref_keys[legend_name]]
                     if ref is None:
-                        print(f"ERROR: no ref found for {legend_name}")
+                        logging.warning(f"No ref found for {legend_name}")
                         ref = value*group_slices[group_name]
 
                     local_ref = ref/slices
@@ -174,17 +190,27 @@ class ComparisonPlot():
                     extra = value-base
                     pct = (value-local_ref)/local_ref*100
                     text_extra.append(f"{pct:+.1f}%")
-                    if extra >= 1:
+                    if pct >= 1:
                         y_base.append(base)
                         y_extra.append(extra)
                     else:
                         y_base.append(value)
-                        y_extra.append(0.001) if abs(pct) > 0.5 else 0
+                        y_extra.append(0.001 if abs(pct) > 0.5 else 0)
+
+                    max_extra_ref_pct = max([max_extra_ref_pct, pct])
 
             is_training = ref_keys[legend_name] == "training"
             secondary_axis = is_training
-            color = "cornflowerblue" if is_training else "darkgreen"
+
             name = "Training" if is_training else "Inference"
+            if "full" in legend_name:
+                color = "blue" if is_training else "green"
+                name = f"Full GPU {name}"
+            else:
+                color = "cornflowerblue" if is_training else "darkgreen"
+                name = f"Fractional {name}"
+
+
             show_legend = "#0" in legend_name
 
             fig.add_trace(
@@ -202,8 +228,9 @@ class ComparisonPlot():
                        x=x_idx, y=y_extra, text=text_extra,
                        marker_line_color='red', marker_line_width=2,
                        width=width-(width*0.1),
+                       legendgroup=name,
                        showlegend=False,
-                            hoverlabel= {'namelength' :-1}),
+                       hoverlabel= {'namelength' :-1}),
                 secondary_y=secondary_axis,
             )
 
@@ -228,7 +255,7 @@ class ComparisonPlot():
                         width=1,
                         dash='dashdot',
                     ),
-                ), secondary_y=False)
+                ), secondary_y=True)
 
         yaxis_title = "<b>{what} speed</b> (in img/s, higher is better)"
         fig.update_layout(title=f"NVDIA Deep Learning SSD AI/ML Processing Speed Comparison<br>using Run:AI GPU fractional GPU", title_x=0.5,
@@ -242,4 +269,10 @@ class ComparisonPlot():
         fig.update_yaxes(showgrid=False, showline=True, linewidth=0.1, linecolor='black')
         fig.update_xaxes(showgrid=False, showline=True, linewidth=0.1, linecolor='black', mirror=True)
 
+
+        training_better_than_ref = (max_values["training"] - ref_values["training"])/ref_values["training"]
+        inference_better_than_ref = (max_values["inference"] - ref_values["inference"])/ref_values["inference"]
+
+        fig.update_yaxes(range=[0, max_values["inference"] * (1.02+training_better_than_ref)], secondary_y=False)
+        fig.update_yaxes(range=[0, max_values["training"] * (1.02+inference_better_than_ref)], secondary_y=True)
         return fig, ""
